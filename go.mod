71cf9469343c93e0d020e551f60f2fd2f9efbe86
sihyeong671 <bshlab671@naver.com>
2024-03-10 17:23:33 +0900

    first webhook commit

---
+++ go.mod
@@ -0,0 +1,32 @@
+module discord_bot
+
+go 1.21.6
+
+require (
+	github.com/PuerkitoBio/goquery v1.9.1 // indirect
+	github.com/andybalholm/cascadia v1.3.2 // indirect
+	github.com/antchfx/htmlquery v1.3.0 // indirect
+	github.com/antchfx/xmlquery v1.3.18 // indirect
+	github.com/antchfx/xpath v1.2.5 // indirect
+	github.com/bwmarrin/discordgo v0.27.1 // indirect
+	github.com/disgoorg/disgo v0.17.2 // indirect
+	github.com/disgoorg/json v1.1.0 // indirect
+	github.com/disgoorg/snowflake/v2 v2.0.1 // indirect
+	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/gocolly/colly v1.2.0 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/gorilla/websocket v1.5.1 // indirect
+	github.com/jawher/mow.cli v1.2.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/saintfish/chardet v0.0.0-20230101081208-5e3ef4b5456d // indirect
+	github.com/sasha-s/go-csync v0.0.0-20240107134140-fcbab37b09ad // indirect
+	github.com/temoto/robotstxt v1.1.2 // indirect
+	golang.org/x/crypto v0.19.0 // indirect
+	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/sys v0.17.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/appengine v1.6.8 // indirect
+	google.golang.org/protobuf v1.32.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/disgoorg/disgo/webhook"
+	"github.com/gocolly/colly"
+	"github.com/joho/godotenv"
+)
+
+// https://1minute-before6pm.tistory.com/42
+
+func main() {
+	env_err := godotenv.Load()
+	if env_err != nil {
+		log.Fatal("Error loading .eng file")
+	}
+	discord_url := os.Getenv("DISCORD_URL")
+
+	client, webhook_err := webhook.NewWithURL(discord_url)
+	if webhook_err != nil {
+		log.Fatal("Error in discord_url")
+	}
+
+	fmt.Println(discord_url)
+	// now := time.Now()
+	web_url := "https://dev-event.vercel.app/events"
+
+	var data = []map[string]string{}
+	collector := colly.NewCollector()
+	collector.OnHTML(".list_wrapper__tpe4x", func(e *colly.HTMLElement) {
+		var msg = map[string]string{}
+
+		e.ForEach(".Item_item__86e_I", func(i int, e_1 *colly.HTMLElement) {
+			var flag bool = false
+			if e_1.ChildText(".DdayTag_tag__YDKTM") == "Today" {
+				flag = true
+			}
+			title := e_1.ChildText(".Item_item__content__title___fPQa")
+			msg["title"] = title
+			tag := e_1.ChildText(".FilterTag_tag__JRNsk")
+			is_contain := strings.Contains(tag, "AI")
+			if is_contain {
+				url := e.ChildAttr("a", "href")
+				msg["desc"] = url + "\n"
+				e_1.ForEach(".Item_wrap__qzpSH", func(i int, e_2 *colly.HTMLElement) {
+					desc_title := e_2.ChildText(".Item_label__vgqgX")
+					desc_content_1 := e_2.ChildText(".Item_host__zNXMy")
+					desc_content_2 := e_2.ChildText(".Item_date__kVMJZ")
+					msg["desc"] += desc_title + " " + desc_content_1 + desc_content_2 + "\n"
+				})
+				if flag {
+					data = append(data, msg)
+				}
+			}
+		})
+	})
+
+	collector.Visit(web_url)
+
+	var content string = "## 진행중인 개발자 행사\n"
+	for _, d := range data {
+		title := d["title"]
+		desc := d["desc"]
+		content += "### " + title + "\n" + desc + "\n"
+	}
+	fmt.Println(content)
+
+	if false {
+		_, err := client.CreateContent(content)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+}
