cdba9e86104a2696741f1c5f26607c25c8a05a2e
mangsgi <aodrntjr0901@naver.com>
2024-11-11 16:19:37 +0900

    refactor: use chromedp

--- go.mod
+++ go.mod
@@ -1,30 +1,22 @@
 module discord_bot
 
-go 1.21.6
+go 1.23.2
 
 require (
-	github.com/disgoorg/disgo v0.17.2
-	github.com/gocolly/colly v1.2.0
-	github.com/joho/godotenv v1.5.1
-)
-
-require (
-	github.com/PuerkitoBio/goquery v1.9.1 // indirect
-	github.com/andybalholm/cascadia v1.3.2 // indirect
-	github.com/antchfx/htmlquery v1.3.0 // indirect
-	github.com/antchfx/xmlquery v1.3.18 // indirect
-	github.com/antchfx/xpath v1.2.5 // indirect
-	github.com/disgoorg/json v1.1.0 // indirect
-	github.com/disgoorg/snowflake/v2 v2.0.1 // indirect
-	github.com/gobwas/glob v0.2.3 // indirect
-	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
-	github.com/golang/protobuf v1.5.3 // indirect
-	github.com/kennygrant/sanitize v1.2.4 // indirect
-	github.com/saintfish/chardet v0.0.0-20230101081208-5e3ef4b5456d // indirect
+	github.com/chromedp/cdproto v0.0.0-20241030022559-23c28aebe8cb // indirect
+	github.com/chromedp/chromedp v0.11.1 // indirect
+	github.com/chromedp/sysutil v1.1.0 // indirect
+	github.com/disgoorg/disgo v0.18.13 // indirect
+	github.com/disgoorg/json v1.2.0 // indirect
+	github.com/disgoorg/snowflake/v2 v2.0.3 // indirect
+	github.com/gobwas/httphead v0.1.0 // indirect
+	github.com/gobwas/pool v0.2.1 // indirect
+	github.com/gobwas/ws v1.4.0 // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/sasha-s/go-csync v0.0.0-20240107134140-fcbab37b09ad // indirect
-	github.com/temoto/robotstxt v1.1.2 // indirect
-	golang.org/x/net v0.21.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
-	google.golang.org/appengine v1.6.8 // indirect
-	google.golang.org/protobuf v1.32.0 // indirect
+	golang.org/x/crypto v0.27.0 // indirect
+	golang.org/x/sys v0.26.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,74 +1,116 @@
 package main
 
 import (
+	"context"
+	"fmt" //ms test
 	"log"
 	"os"
 	"strings"
+	"time"
 
-	"github.com/disgoorg/disgo/webhook"
-	"github.com/gocolly/colly"
+	"github.com/chromedp/cdproto/cdp"
+	"github.com/chromedp/chromedp"
+	"github.com/disgoorg/disgo/webhook" //Discord 관련
 	"github.com/joho/godotenv"
 )
 
 // https://1minute-before6pm.tistory.com/42
 
 func main() {
-	env_err := godotenv.Load()
+	env_err := godotenv.Load() //Discord 관련
 	if env_err != nil {
 		log.Fatal("Error loading .eng file")
 	}
 	discord_url := os.Getenv("DISCORD_URL")
 
-	client, webhook_err := webhook.NewWithURL(discord_url)
+	client, webhook_err := webhook.NewWithURL(discord_url) //Discord 관련
 	if webhook_err != nil {
 		log.Fatal("Error in discord_url")
 	}
 
-	// now := time.Now()
+	// now := time.Now() //Discord 관련
 	web_url := "https://dev-event.vercel.app/events"
 
-	var data = []map[string]string{}
-	collector := colly.NewCollector()
-	collector.OnHTML(".list_wrapper__tpe4x", func(e *colly.HTMLElement) {
+	var data []map[string]string
+
+	ctx, cancel := chromedp.NewContext(context.Background()) //, chromedp.WithDebugf(log.Printf)) // to debugging
+	defer cancel()
+
+	// Visit the webpage and extract the relevant information
+	var innerNodes []*cdp.Node
+	err_4 := chromedp.Run(ctx,
+		chromedp.Navigate(web_url),
+		chromedp.Sleep(3*time.Second), // 페이지 로딩을 위한 시간 대기
+		chromedp.WaitVisible(".Home_section__EaDnq", chromedp.ByQuery),
+		chromedp.Nodes(".Item_item__container___T09W", &innerNodes, chromedp.ByQueryAll),
+	)
+	if err_4 != nil {
+		log.Fatal("Error HTML", err_4)
+	}
+
+	for _, node := range innerNodes {
+		var todayText string
+		err_1 := chromedp.Run(ctx,
+			chromedp.Text(".DdayTag_tag__6_oE7", &todayText, chromedp.ByQuery, chromedp.FromNode(node)),
+		)
+		if err_1 != nil {
+			log.Println("Error extracting Today flag:", err_1)
+			continue
+		} else if !strings.Contains(todayText, "Today") {
+			continue
+		}
+
+		var tagNodes []*cdp.Node
+		err_2 := chromedp.Run(ctx,
+			chromedp.Nodes(".FilterTag_tag__etNfv", &tagNodes, chromedp.ByQueryAll, chromedp.FromNode(node)),
+		)
+		if err_2 != nil {
+			log.Println("Error extracting Tag:", err_2)
+			continue
+		}
+		isAI := false
+		for _, tagNode := range tagNodes {
+			// fmt.Println(tagNode.Children[0].NodeValue)
+			if strings.Contains(tagNode.Children[0].NodeValue, "AI") {
+				isAI = true
+				break
+			}
+		}
+		fmt.Println()
+		if !isAI {
+			continue
+		}
+
 		var msg = map[string]string{}
+		var titleText, urlText, dateText, hostText string
+		err_3 := chromedp.Run(ctx,
+			chromedp.Text(".Item_item__content__title__94_8Q", &titleText, chromedp.ByQuery, chromedp.FromNode(node)),
+			chromedp.AttributeValue("a", "href", &urlText, nil, chromedp.FromNode(node)),
+			chromedp.Text(".Item_date__date__CoMqV", &dateText, chromedp.ByQuery, chromedp.FromNode(node)),
+			chromedp.Text(".Item_host__3dy8_", &hostText, chromedp.ByQuery, chromedp.FromNode(node)),
+		)
+		if err_3 != nil {
+			log.Println("Error extracting title or url:", err_3)
+			continue
+		}
+		msg["title"] = titleText
+		msg["desc"] = urlText + "\n" + "주최: " + hostText + "\n" + "모집: " + dateText
 
-		e.ForEach(".Item_item__86e_I", func(i int, e_1 *colly.HTMLElement) {
-			var flag bool = false
-			if e_1.ChildText(".DdayTag_tag__YDKTM") == "Today" {
-				flag = true
-			}
-			title := e_1.ChildText(".Item_item__content__title___fPQa")
-			msg["title"] = title
-			tag := e_1.ChildText(".FilterTag_tag__JRNsk")
-			is_contain := strings.Contains(tag, "AI")
-			if is_contain {
-				url := e.ChildAttr("a", "href")
-				msg["desc"] = url + "\n"
-				e_1.ForEach(".Item_wrap__qzpSH", func(i int, e_2 *colly.HTMLElement) {
-					desc_title := e_2.ChildText(".Item_label__vgqgX")
-					desc_content_1 := e_2.ChildText(".Item_host__zNXMy")
-					desc_content_2 := e_2.ChildText(".Item_date__kVMJZ")
-					msg["desc"] += desc_title + " " + desc_content_1 + desc_content_2 + "\n"
-				})
-				if flag {
-					data = append(data, msg)
-				}
-			}
-		})
-	})
-
-	collector.Visit(web_url)
+		data = append(data, msg)
+	}
 
 	var content string = "## 진행중인 개발자 행사\n"
 	for _, d := range data {
-		title := d["title"]
-		desc := d["desc"]
-		content += "### " + title + "\n" + desc + "\n"
+		content += "###" + d["title"] + "\n"
+		content += d["desc"] + "\n\n"
 	}
 
-	_, err := client.CreateContent(content)
-	if err != nil {
-		log.Fatal(err)
-	}
+	fmt.Println("Webhook content preview:\n", content) //ms test
+	fmt.Println("노드 개수: ", len(innerNodes))            //ms test
+	fmt.Println("필터링된 노드 개수: ", len(data))             //ms test
 
+	_, err_4 = client.CreateContent(content)
+	if err_4 != nil {
+		log.Fatal(err_4)
+	}
 }
